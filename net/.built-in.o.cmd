06bc6f07a8440ab9d4bb10778e24ef181d5867b1
xuguangmin <1312909726@qq.com>
2015-08-27 12:14:50 +0800

    网络相关代码,实现了各种常见的网络协议,如TCP/IP,IPX等

---
+++ net/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_net/built-in.o :=  arm-linux-ld -EL    -r -o net/built-in.o net/nonet.o net/wireless/built-in.o net/ieee802154/built-in.o 
---
+++ net/wireless/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_net/wireless/built-in.o :=  rm -f net/wireless/built-in.o; arm-linux-ar rcs net/wireless/built-in.o
