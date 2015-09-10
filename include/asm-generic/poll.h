75f2ccbc65b6a483b25bd4cd938b8b40ab737a80
xuguangmin <1312909726@qq.com>
2015-09-10 13:53:41 +0800

    同步阻塞/非阻塞型的I/O操作方式poll的实现机制

--- include/asm-generic/poll.h
+++ include/asm-generic/poll.h
@@ -1,6 +1,39 @@
 #ifndef __ASM_GENERIC_POLL_H
 #define __ASM_GENERIC_POLL_H
 
+/**
+ *	EXAMPLE:驱动程序实现的poll例程的示例
+ 	//定义一个用于读取的等待队列demo_inq
+	static DECLARE_WAIT_QUEUE_HEAD(demo_inq);
+
+	//驱动程序实现的poll例程
+	unsigned int demo_poll(struct file *filp, struct poll_table_struct * wait)
+	{
+		struct demo_buf_list * list = filp->private_data;
+		//初始化mask为0,表明目前关于设备的数据的状态没有发生任何变化
+		unsigned int mask = 0;
+		...
+		//调用poll_wait将来自内核中的一个等待节点加入demo_inq队列
+		poll_wait(filp, &demo_inq, wait);
+		//判断缓冲区是否可读
+		if(list->head != list->tail)
+			mask |= POLLIN|POLLRDNORM;
+		return mask;
+	}
+	//设备驱动程序实现的中断处理例程
+	irqreturn_t demo_isr(int irq, void * dev_id)
+	{
+		...
+		//如果缓冲区可读,调用wake_up函数唤醒阻塞在poll上的进程 
+		wake_up_interruptible(&demo_inq);
+		...
+	}
+ */
+
+
+
+
+
 /* These are specified by iBCS2 */
 /*poll例程状态位*/
 #define POLLIN		0x0001	/*非高优先级的数据(带外数据out-of_band)可以被无阻塞的读取*/
